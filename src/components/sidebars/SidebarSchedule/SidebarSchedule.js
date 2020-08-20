843c41280956b2e424b11e48953fbf14f2fd49f5
wns312 <wns312@gmail.com>
2020-08-20 16:13:57 +0900

    캘린더 수정 페이지 8할 완성

--- src/components/sidebars/SidebarSchedule/SidebarSchedule.js
+++ src/components/sidebars/SidebarSchedule/SidebarSchedule.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './SidebarSchedule.module.css'
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ko from 'date-fns/locale/ko'; 
+registerLocale('ko', ko);
 
 const SidebarSchedule = ({currentEvent, setCurrentEvent}) => {
   let {_id, title, desc, start, end} = currentEvent
+
+  const [Title, setTitle] = useState(title);
+  const [Desc, setDesc] = useState(desc);
+  const [startDate, setStartDate] = useState(start);
+  const [endDate, setEndDate] = useState(end);
+
+  useEffect(()=>{
+    setTitle(currentEvent.title);
+    setDesc(currentEvent.desc);
+    setStartDate(currentEvent.start)
+    setEndDate(currentEvent.end)
+  }, [currentEvent])
+
   function Close(e) {
     setCurrentEvent("");
   }
+  function handleInput(e) {
+    setTitle(e.target.value);
+  }
+  function handletextArea(e) {
+    setDesc(e.target.value);
+  }
+  function handleSubmit() {
+    console.log(Title);
+    console.log(Desc);
+    console.log(startDate);
+    console.log(endDate);
+    
+  }
+
+
   return (
     <div id={styles.aside}>
     <section id={styles.aside_header}>
@@ -16,15 +48,30 @@ const SidebarSchedule = ({currentEvent, setCurrentEvent}) => {
       </svg>
     </section>
     <section id={styles.aside_body}>
-      <input id={styles.event_title} placeholder="제목" defaultValue={title}></input>
+      <input id={styles.event_title} placeholder="제목" value={Title} onChange={handleInput}></input>
       <hr/>
-      <textarea id={styles.event_desc} placeholder="내용" defaultValue={desc}></textarea>
+      <textarea id={styles.event_desc} placeholder="내용" value={Desc} onChange={handletextArea}></textarea>
       <hr/>
-      {start.getTime()} <br/>
+      {/* {start.getTime()} <br/>
       {end.getTime()} <br/>
       {start.toDateString()} <br/>
       {end.toTimeString()} <br/>
       {end.toString()} <br/>
+      {new Date().toString()} <br/> */}
+      <DatePicker
+        locale="ko"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        placeholderText="시작날짜"
+      />
+      <DatePicker
+        locale="ko"
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        placeholderText="끝날짜"
+      />
+      <div id={styles.submit} onClick={handleSubmit}>저장</div>
+      {/* 새로만들면 저장, 수정하면 수정 */}
     </section>
   </div>
   );
