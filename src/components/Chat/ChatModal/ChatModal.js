ead5bce0c07f44d3c435dba760b2b8721211b2df
wns312 <wns312@gmail.com>
2020-08-26 11:28:01 +0900

    채팅모달에 임티삽입

--- src/components/Chat/ChatModal/ChatModal.js
+++ src/components/Chat/ChatModal/ChatModal.js
@@ -5,10 +5,10 @@ const ChatModal = ({modalPosition, setColor,removeColor}) => {
   let {offsetLeft, offsetWidth, offsetHeight} = modalPosition
   return (
       <section className={styles.content} style={{top:offsetHeight-10, left : offsetLeft+offsetWidth-170}} onMouseOver={setColor} onMouseLeave={removeColor}>
-        <article className={styles.content__body}></article>
-        <article className={styles.content__body}></article>
-        <article className={styles.content__body}></article>
-        <article className={styles.content__body}></article>
+        <i className={`far fa-laugh ${styles.content__body}`}></i>
+        <i className={`fas fa-edit ${styles.content__body}`}></i>
+        <i className={`fas fa-trash-alt ${styles.content__body}`}></i>
+        <i className={`fas fa-ellipsis-h ${styles.content__body}`} style={{padding : '12px 0 0 11.7px'}}></i>
       </section>
   );
 };
