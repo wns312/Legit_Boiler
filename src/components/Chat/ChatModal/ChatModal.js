49e52077c6536c601a9caac4ff46e52ed7d43f30
wns312 <wns312@gmail.com>
2020-08-27 15:58:50 +0900

    모달 갯수에 맞춘 범위 완료

--- src/components/Chat/ChatModal/ChatModal.js
+++ src/components/Chat/ChatModal/ChatModal.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from'./ChatModal.module.css';
+import { useSelector } from 'react-redux';
 
 const ChatModal = ({message, Modify, Delete}) => {
+  let { _id } = useSelector(state => state.user.userData)
+  let {userId} = message
+  console.log(userId)
+  console.log(_id);
   return (
+    <div className={styles.content_wrapper}>
       <section className={styles.content}>
         <i className={`far fa-laugh ${styles.content__body}` }></i>
-        <i className={`fas fa-edit ${styles.content__body}`} onClick={Modify}></i>
-        <i className={`fas fa-trash-alt ${styles.content__body}`} onClick={Delete}></i>
-        <i className={`fas fa-ellipsis-h ${styles.content__body}`} style={{padding : '12px 0 0 11.7px'}}></i>
+        {userId === _id && <i className={`fas fa-edit ${styles.content__body}`} onClick={Modify}></i>}
+        {userId === _id && <i className={`fas fa-trash-alt ${styles.content__body}`} onClick={Delete}></i>}
+        <i className={`fas fa-ellipsis-h ${styles.content__body}`} ></i>
       </section>
+    </div>
   );
 };
 
