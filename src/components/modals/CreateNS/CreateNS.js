9651f000596d2d6fe001f3963e0a207d2a5b1ee9
wns312 <wns312@gmail.com>
2020-09-17 17:03:47 +0900

    네임스페이스, 방 regex 완료

--- src/components/modals/CreateNS/CreateNS.js
+++ src/components/modals/CreateNS/CreateNS.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Input, Modal } from 'semantic-ui-react'
 import { useSelector } from 'react-redux';
+import styles from './CreateNS.module.css';
 
 const CreateNS = ({Socket}) => {
   let {_id} = useSelector(state=>state.user.userData)  //유저아이디
@@ -8,6 +9,7 @@ const CreateNS = ({Socket}) => {
   const [Open, setOpen] = useState(false);
   const [Size, setSize] = useState();
   const [nsTitle, setNsTitle] = useState("");
+  const [hidden, setHidden] = useState(true);
 
   function show(size) {
     setSize(()=>size);
@@ -16,14 +18,30 @@ const CreateNS = ({Socket}) => {
 
   function close() {
     setOpen(false);
+    setHidden(true);
+    setNsTitle("");
+  }
+  //NS 정규표현식
+  function chkNS(){
+    let isNS = /^[가-힣a-zA-Z0-9]([_]?[가-힣a-zA-Z0-9]){2,20}$/;
+    return isNS.test(nsTitle) ? true : false;
   }
 
   function createNs(e){
     e.preventDefault();
-    //axios요청으로 뺄 수 있나?
-    Socket.emit("NewNs", {nsTitle, _id});
-    setOpen(false);
     setNsTitle("");
+    
+    let ns = chkNS()
+    if(ns) {
+      Socket.emit("NewNs", {nsTitle, _id});
+      setOpen(false);
+      setHidden(true)
+    }
+    else {
+      setOpen(true);
+      setNsTitle(nsTitle);
+      setHidden(false)
+    }
   }
 
   function handleNstitle(event) {
@@ -39,6 +57,9 @@ const CreateNS = ({Socket}) => {
             &emsp;생성할 네임스페이스 이름을 입력하세요 <hr/>
             <form onSubmit={createNs}>
             &emsp;<Input focus value={nsTitle} onChange={handleNstitle} placeholder="네임스페이스 입력" />
+            <br/>&emsp;{hidden 
+            ? <span className={styles.check_visible}>2~20자의 한글과 영문, 언더바(_)만 사용가능합니다.</span>
+            :  <span className={styles.check_hidden}>_를 제외한 특수문자는 사용할 수 없습니다.</span>}
             </form>
           </Modal.Content>
           <Modal.Actions>
--- src/components/modals/CreateRoom/CreateRoom.js
+++ src/components/modals/CreateRoom/CreateRoom.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, Input, Checkbox } from 'semantic-ui-react'
 import {useSelector} from 'react-redux';
+import styles from './CreateRoom.module.css';
 
 const CreateRoom = ({children}) => {
   let {_id} = useSelector(state=>state.user.userData) //유저아이디
@@ -9,31 +10,48 @@ const CreateRoom = ({children}) => {
   const [Size, setSize] = useState();
   const [roomTitle, setRoomTitle] = useState("");
   const [isPrivate, setIsPrivate] = useState(false);
+  const [hidden, setHidden] = useState(true);
+
   function show(size) {
     setSize(size);
     setOpen(true);
   }
-  function close() { setOpen(false); }
+  function close() { 
+    setOpen(false); 
+    setHidden(true);
+    setRoomTitle("");
+  }
   function handlePrivate(e) {
     setIsPrivate(value=>!value)
   }
   function handleRoomTitle(event) {
     setRoomTitle(event.target.value)
   }
+  //방이름 정규표현식
+  function chkRoom(){
+    let isNS = /^[가-힣a-zA-Z0-9]([_]?[가-힣a-zA-Z0-9]){2,20}$/;
+    return isNS.test(roomTitle) ? true : false;
+  }
 
   function createRoom(e){
     e.preventDefault();
-    //여기서 받아온 데이터로 REST요청을 보내서 방을 생성할 것 (중복검사도 해야됨)
-    //+ 네임스페이스 이름을 알아야 조회해서 push하고 업데이트 할 것
-    //isPrivate여부에 따라서 data를 다르게 emit할 것?
-    if(isPrivate){
-      nsSocket.emit("NewRoom", {roomTitle, isPrivate, _id, Ns_id : currentNs._id})
-    }else{
-      let ids = currentNs.nsMember.map(person=>person._id)
-      nsSocket.emit("NewRoom", {roomTitle, isPrivate, ids, Ns_id : currentNs._id})
-    }
-    setOpen(false);
     setRoomTitle("");
+
+    let room = chkRoom();
+    if(room){
+      if(isPrivate){
+        nsSocket.emit("NewRoom", {roomTitle, isPrivate, _id, Ns_id : currentNs._id})
+      }else{
+        let ids = currentNs.nsMember.map(person=>person._id)
+        nsSocket.emit("NewRoom", {roomTitle, isPrivate, ids, Ns_id : currentNs._id})
+      }
+      setOpen(false)
+      setHidden(true)
+    } else {
+      setOpen(true)
+      setRoomTitle(roomTitle)
+      setHidden(false)
+    }
   }
 
   return (
@@ -46,6 +64,10 @@ const CreateRoom = ({children}) => {
             <form onSubmit={createRoom}>
             &emsp;<Input focus value={roomTitle} onChange={handleRoomTitle} placeholder="방 이름" />
               &emsp;<Checkbox label='비밀방' checked={isPrivate} onClick={handlePrivate} toggle/>
+              <br />&emsp;
+              {hidden 
+            ? <span className={styles.check_visible}>2~20자의 한글과 영문, 언더바(_)만 사용가능합니다.</span>
+            :  <span className={styles.check_hidden}>_를 제외한 특수문자는 사용할 수 없습니다.</span>}
             </form>
           </Modal.Content>
           <Modal.Actions>
