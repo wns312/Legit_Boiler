08156a58bc0d8b4569e60d755f5d750fc11e4de5
wns312 <wns312@gmail.com>
2020-10-04 16:09:43 +0900

    기본 서식 완료

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './About.module.css'
+import {Container, Header, Divider, Segment } from 'semantic-ui-react'
+
 import Nav from '../Nav/Nav';
 
 const About = () => {
     return (
         <>
             <Nav></Nav>
-            <div>
-                AboutPage
-            </div>
+            <Container text style={{ marginTop: '7em' }}>
+            <Header as='h1'>Semantic UI React Fixed Template</Header>
+      <p>This is a basic fixed menu template using fixed size containers.
+          This is a basic fixed menu template using fixed size containers.
+          This is a basic fixed menu template using fixed size containers.
+          This is a basic fixed menu template using fixed size containers.</p>
+            </Container>
+            <Container inverted vertical style={{ margin: '5em 0em 0em'}}>
+                <Divider inverted section />
+            </Container>
         </>
     );
 };
