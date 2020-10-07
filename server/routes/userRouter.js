ecf337ce348a0179ca3c01e28dbd0974cc5f9436
wns312 <wns312@gmail.com>
2020-10-07 18:30:59 +0900

    최종 완성본

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
-const multer = require('multer');
-const path = require('path');
-const { User } = require('../models/User');
-const { auth } = require('../middleware/auth');
+const multer = require("multer");
+const path = require("path");
+const { User } = require("../models/User");
+const { auth } = require("../middleware/auth");
 
-
-let storage = multer.diskStorage({ // 파일이 저장될 스토리지경로와 저장될 파일명 지정
-  destination: (req, file, callback)=>{ // 경로설정
+let storage = multer.diskStorage({
+  // 파일이 저장될 스토리지경로와 저장될 파일명 지정
+  destination: (req, file, callback) => {
+    // 경로설정
     callback(null, `uploads/`); // 루트경로.uploads를 경로로 지정하겠다
   },
-  filename: (req, file, callback)=>{   //파일이름설정 : 현재시_원래이름 으로 하였음
+  filename: (req, file, callback) => {
+    //파일이름설정 : 현재시_원래이름 으로 하였음
     callback(null, `${Date.now()}_${file.originalname}`);
   },
   fileFilter: (req, file, callback) => {
@@ -22,63 +24,84 @@ let storage = multer.diskStorage({ // 파일이 저장될 스토리지경로와 
       return callback(new Error("only jpg, png is allowed"));
     }
     callback(null, true);
-  }
-})
-const upload = multer({storage : storage}).single("file"); // 지정된 storage로 upload함수 생성
-
-
+  },
+});
+const upload = multer({ storage: storage }).single("file"); // 지정된 storage로 upload함수 생성
 
 //여기는 유저
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
   user.save((err, userInfo) => {
-    if (err) { return res.json({ success: false, err }) }
+    if (err) {
+      return res.json({ success: false, err });
+    }
     return res.status(200).json({
-      success: true, userInfo}) // ??
+      success: true,
+      userInfo,
+    }); // ??
   });
-})
+});
 
 router.post("/login", (req, res) => {
   User.findOne({ email: req.body.email }, function (err, user) {
-    if (!user) return res.json({ loginSuccess: false, message: "이메일에 해당하는 유저가 없습니다" });
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "이메일에 해당하는 유저가 없습니다",
+      });
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 일치하지 않습니다" });
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 일치하지 않습니다",
+        });
       user.generateToken((err, user) => {
-        let {_id, name, image, role, email, token} = user
-        res.cookie("x_auth", token).status(200)
-          .json({ loginSuccess: true, _id, name, image, role, email })
+        let { _id, name, image, role, email, token } = user;
+        res
+          .cookie("x_auth", token)
+          .status(200)
+          .json({ loginSuccess: true, _id, name, image, role, email });
       });
     });
     if (!user.isVerified) {
       if (err) res.json({ success: false, err });
-      else {sendingEmail(user); }
+      else {
+        sendingEmail(user);
+      }
     }
-  })
-  .catch((err)=>{
+  }).catch((err) => {
     return res.status(400).send(err);
-  })
+  });
 });
 
-router.get('/auth', auth, (req, res) => {
-  let {_id, role, email, name, image, password, isVerified, isSns} = req.user
+router.get("/auth", auth, (req, res) => {
+  let { _id, role, email, name, image, password, isVerified, isSns } = req.user;
   res.status(200).json({
-    _id, email, name, image, password, isVerified, isSns,
+    _id,
+    email,
+    name,
+    image,
+    password,
+    isVerified,
+    isSns,
     isAdmin: role === 0 ? false : true,
-    isAuth: true
-  })
-})
+    isAuth: true,
+  });
+});
 //로그아웃
-router.get('/logout', auth, (req, res)=>{
+router.get("/logout", auth, (req, res) => {
   //유저를 찾아왔을테니 업데이트 해주어야 한다
-  User.findOneAndUpdate({_id : req.user._id}, {token : ""}, (err, user)=>{
-    if(err) return res.json({success : false, err})
-    return res.status(200).send({success : true})
-  })
-})
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
 
 router.get("/getValidation", auth, (req, res) => {
-  User.findByIdAndUpdate( { _id: req.user._id }, { isVerified: true },
+  User.findByIdAndUpdate(
+    { _id: req.user._id },
+    { isVerified: true },
     (err, user) => {
       if (err) return res.json({ success: false, err });
       return res.status(200).send({ success: true });
@@ -90,30 +113,32 @@ router.get("/resend", auth, (req, res) => {
   User.findOne({ email: req.user.email }, (err, user) => {
     sendingEmail(user);
     return res.status(200).send({ success: true });
-  })
-  .catch((err)=>{
+  }).catch((err) => {
     res.json({ success: false, err });
-  })
+  });
 });
 
-router.post("/modify", auth, (req, res)=>{
-  let {user, body} = req
-  let {password, image, name} = body
+router.post("/modify", auth, (req, res) => {
+  let { user, body } = req;
+  let { password, image, name } = body;
   User.findOne({ _id: user.id }, (err, user) => {
     if (err) return res.json({ success: false, err });
-    User.updateOne( {_id: user._id}, { $set: { password, image, name } }, (err,userInfo)=>{
-      if(err) return res.json({success: false, err})
-      return res.status(200).send({ success:true, user : userInfo })
+    User.updateOne(
+      { _id: user._id },
+      { $set: { password, image, name } },
+      (err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({ success: true, user: userInfo });
       }
-    )
-  })
-})
+    );
+  });
+});
 
 router.post("/uploadImage", (req, res) => {
   console.log("uploadImage");
-  upload(req, res, err => {
+  upload(req, res, (err) => {
     if (err) {
-      console.log("업로드 에러 : "+err);
+      console.log("업로드 에러 : " + err);
       return res.json({ success: false, err });
     }
     return res.json({
@@ -124,48 +149,56 @@ router.post("/uploadImage", (req, res) => {
   });
 });
 
-
-
-router.post("/nopassmodify", auth, (req, res)=>{
-  console.log("auth 노패스 모디파이", req.user)
+router.post("/nopassmodify", auth, (req, res) => {
+  console.log("auth 노패스 모디파이", req.user);
   User.findOne({ _id: req.user.id }, (err, user) => {
-    console.log("파인드원",user)
-  if (err) return res.json({ success: false, err });
+    console.log("파인드원", user);
+    if (err) return res.json({ success: false, err });
     // console.log("user._id 아이디아이디", user._id)
-    console.log("req.user 유저유저",req.user)
-    console.log("req.body 바디바디",req.body)
+    console.log("req.user 유저유저", req.user);
+    console.log("req.body 바디바디", req.body);
     //req.body를 확인하기
-  User.updateOne(
-    {_id: user._id},
-    {//$set을 해야 해당 필드만 바뀝니다. https://www.zerocho.com/category/MongoDB/post/579e2821c097d015000404dc
-      $set: {       //req.body => body로 보내고
-        image: req.body.newImage,
-        name: req.body.newName,
-            },
-    },
-    console.log("req바디 패스워드",req.body.newPassword),
-    (err,userInfo)=>{
-      if(err) return res.json({success: false, err})
-      return res.status(200).send({
-        success:true,
-        user : userInfo,
-        })
+    User.updateOne(
+      { _id: user._id },
+      {
+        //$set을 해야 해당 필드만 바뀝니다. https://www.zerocho.com/category/MongoDB/post/579e2821c097d015000404dc
+        $set: {
+          //req.body => body로 보내고
+          image: req.body.newImage,
+          name: req.body.newName,
+        },
+      },
+      console.log("req바디 패스워드", req.body.newPassword),
+      (err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+          success: true,
+          user: userInfo,
+        });
       }
-    )
-  })
-})
+    );
+  });
+});
 
-module.exports =router
+module.exports = router;
 
 function sendingEmail(user) {
   const Verifiedtoken = jwt.sign(user._id.toHexString(), "registerToken");
   const url2 = `http://localhost:3000/valid/${Verifiedtoken}`;
   let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { user: "bitbitlegit@gmail.com",  pass: "!bit9000" },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "bitbitlegit@gmail.com",
+      pass: "bhcyuadpcxxfibkw",
+    },
   });
+
   const mailOptions = {
-    from: "bitbitlegit@gmail.com", to: user.email, subject: "안녕하세요, 이메일 인증을 해주세요.",
+    from: "bitbitlegit@gmail.com",
+    to: user.email,
+    subject: "안녕하세요, 이메일 인증을 해주세요.",
     html: `Please, confirm your email by clicking the following link: <a href=${url2}>${url2}</a>`,
   };
   transporter.sendMail(mailOptions, function (error, info) {});
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,18 +10,18 @@ const About = () => {
             <Nav></Nav>
             <Container text style={{ marginTop: '7em' }}>
             <Header as='h1'>조원 구성</Header>
-            <div>
-            <Image src="/default_profile.png" size='small' floated='left' alt="oh" verticalAlign='top'/>
+           <br/> <div>
+            <Image src="/2.jpg" size='small' floated='left' alt="oh" verticalAlign='middle' circular className={styles.img} />
             <p className={`${styles.body} ${styles.name}`}>김준영</p>
-      <p className={styles.body}>This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.</p>
+      <p className={styles.body}>
+            전반적인 개발일정 총괄과 전반적인 개발을 담당했습니다.
+React, Redux, MongoDB, Socket.io, Express, Node.js를 다루어 보았습니다.
+특히 프로젝트에서 Socket.io와 데이터베이스를 중점적으로 다루었습니다.</p><br/>
           </div>
           <div>
           <hr style={{ marginTop : "30px", width:"4rem", border : "2px solid", backgroundColor : "rgb(80, 80, 80)", borderColor: "rgb(80, 80, 80)"}} />
           <p className={`${styles.body} ${styles.name}`}>하고싶은 말</p>
-          <p className={styles.quote}>"아자아자화이팅!!"</p>
+          <p className={styles.quote}>"많은 것을 배울 수 있었고, 시간 관계상 구현하지 못한 부분이 많은 점이 아쉽습니다"</p>
           </div>
             </Container>
             <Container style={{ margin: '5em 0em 0em'}}>
@@ -29,18 +29,18 @@ const About = () => {
             </Container>
 
             <Container text style={{ marginTop: '1em' }}>
-            <div>
-            <Image src="/default_profile.png" size='small' floated='left' alt="oh" verticalAlign='top'/>
-            <p className={`${styles.body} ${styles.name}`}>김준영</p>
-      <p className={styles.body}>This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.</p>
+            <br/><div>
+            <Image src="/1.jpg" size='small' floated='left' alt="oh" verticalAlign='top' circular className={styles.img}/>
+            <p className={`${styles.body} ${styles.name}`}>김준호</p>
+      <p className={styles.body}>
+            이메일 인증과, 유저 인증에 중점을 두었습니다.
+오픈 API를 이용한 일정관리, MongoDB를 이용한 DB설계를 중점적으로 다루었습니다.
+React, Node.js, MongoDB, Express를 다루어보았습니다.</p><br/>
           </div>
           <div>
           <hr style={{ marginTop : "30px", width:"4rem", border : "2px solid", backgroundColor : "rgb(80, 80, 80)", borderColor: "rgb(80, 80, 80)"}} />
           <p className={`${styles.body} ${styles.name}`}>하고싶은 말</p>
-          <p className={styles.quote}>"아자아자화이팅!!"</p>
+          <p className={styles.quote}>"어려운 점도 많고 아쉬운 점도 있지만, 많은 것을 배울 수 있는 유익한 시간이었습니다"</p>
           </div>
             </Container>
             <Container style={{ margin: '5em 0em 0em'}}>
@@ -48,18 +48,18 @@ const About = () => {
             </Container>
 
             <Container text style={{ marginTop: '1em' }}>
-            <div>
-            <Image src="/default_profile.png" size='small' floated='left' alt="oh" verticalAlign='top'/>
-            <p className={`${styles.body} ${styles.name}`}>김준영</p>
-      <p className={styles.body}>This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.</p>
+            <br/><div>
+            <Image src="/3.jpg" size='small' floated='left' alt="oh" verticalAlign='top' circular className={styles.img}/>
+            <p className={`${styles.body} ${styles.name}`}>김태인</p>
+      <p className={styles.body}>
+            오픈 api를 이용해서 소셜로그인을 구현하였고 입력창에 대한 유효성 검사를 적용하였습니다.
+또한 UI/UX 수정을 담당했습니다.
+React, MongoDB, Express, Node.js를 다룰 수 있습니다.</p><br/>
           </div>
           <div>
           <hr style={{ marginTop : "30px", width:"4rem", border : "2px solid", backgroundColor : "rgb(80, 80, 80)", borderColor: "rgb(80, 80, 80)"}} />
           <p className={`${styles.body} ${styles.name}`}>하고싶은 말</p>
-          <p className={styles.quote}>"아자아자화이팅!!"</p>
+          <p className={styles.quote}>"React, Node.js, MongoDB에 관련 많은 것을 배울 수 있었고, 시간 분배를 잘 하지 못한 점이 아쉽습니다."</p>
           </div>
             </Container>
             <Container style={{ margin: '5em 0em 0em'}}>
@@ -67,18 +67,18 @@ const About = () => {
             </Container>
 
             <Container text style={{ marginTop: '1em' }}>
-            <div>
-            <Image src="/default_profile.png" size='small' floated='left' alt="oh" verticalAlign='top'/>
-            <p className={`${styles.body} ${styles.name}`}>김준영</p>
-      <p className={styles.body}>This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.
-          This is a basic fixed menu template using fixed size containers.</p>
+            <br/><div>
+            <Image src="/4.jpg" size='small' floated='left' alt="oh" verticalAlign='top' circular className={styles.img}/>
+            <p className={`${styles.body} ${styles.name}`}>황승빈</p>
+      <p className={styles.body}>
+            소켓을 이용한 채팅 구현에 중점을 두었습니다.
+git과 github를 이용한 버전관리와 UX/UI를 중점적으로 다루었습니다.
+React, Git, Github, Socket.io, Node.js를 다루어 보았습니다.</p><br/>
           </div>
           <div>
           <hr style={{ marginTop : "30px", width:"4rem", border : "2px solid", backgroundColor : "rgb(80, 80, 80)", borderColor: "rgb(80, 80, 80)"}} />
           <p className={`${styles.body} ${styles.name}`}>하고싶은 말</p>
-          <p className={styles.quote}>"아자아자화이팅!!"</p>
+          <p className={styles.quote}>"프로젝트가 마무리 되어 속이 후련하고, 더 많이 배우고 싶습니다"</p>
           </div>
             </Container>
             <br/>
